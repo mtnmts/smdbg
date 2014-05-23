3fb30ef2f7439c30189497b6dbe0b1d6ef564cfc
Matan M. Mates <matan@matan.me>
2014-05-23 06:52:18 -0400

    Working on http_server.py

---
+++ src/http_server.py
@@ -0,0 +1,29 @@
+# Author  : Matan M. Mates
+# File    : http_server.py
+# Purpose : HTTP Server serving the web application
+
+class HTTPServer(object):
+    import subprocess
+    from tornado.web import WebHandler, StaticFileHandler
+    
+    SERVE_PATH = "web" + os.sep
+
+    def __init__(self, port, serve_path):
+        self._port = port
+        self._running = False
+
+    @property
+    def port(self):
+        return self._port
+         
+
+    def start(self):
+        """ Starts the HTTP Server if it wasn't running,
+            calling this function if the server is already
+            running will do nothing """
+        settings = {'static_path' : SERVE_PATH}
+        handlers = [(r'/', WebHandler),
+                    (r'/*', StaticFileHandler, {'path' : SERVE_PATH})]      
+    @property
+    def running():
+        return self._running
