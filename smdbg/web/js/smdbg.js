917d43d2a765e81bdc961b39cfb89d2f5258020d
Matan Mates <matan@matan.me>
2014-05-24 12:37:40 +0300

    websocket set up proper

--- smdbg/web/js/smdbg.js
+++ smdbg/web/js/smdbg.js
@@ -1,5 +1,16 @@
-var connection = new WebSocket('ws://127.0.0.1/ws', ['soap', 'xmpp']);
+var connection = new WebSocket('ws://127.0.0.1:8080/ws');
 
+// When the connection is open, send some data to the server
 connection.onopen = function () {
-      connection.send('Ping'); // Send the message 'Ping' to the server
+  connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+  console.log('Server: ' + e.data);
 };
